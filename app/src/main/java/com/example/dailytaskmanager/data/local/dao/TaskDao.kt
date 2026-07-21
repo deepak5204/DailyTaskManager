6a1b81b5f6e49f759cbfc535a96f3226c9dd175e
deepak <pdeepak1255@gmail.com>
2026-07-21 13:24:54 +0530

    feat: create Dao for Task

---
+++ app/src/main/java/com/example/dailytaskmanager/data/local/dao/TaskDao.kt
@@ -0,0 +1,32 @@
+package com.example.dailytaskmanager.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.dailytaskmanager.data.local.entity.Task
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+
+interface TaskDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTask(task: Task)
+
+    @Update
+    suspend fun updateTask(task: Task)
+
+    @Delete
+    suspend fun deleteTask(task: Task)
+
+    @Query("SELECT * FROM Task ORDER BY createdAt DESC")
+    suspend fun getAllTask(): Flow<List<Task>>
+
+    @Query("select * from Task where id =:taskId")
+    suspend fun getTaskById(taskId: Long): Task?
+
+    @Query("DELETE FROM Task")
+    suspend fun deleteAllTasks()
+}
